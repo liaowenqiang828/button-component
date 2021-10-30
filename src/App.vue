b67699f957c54bb5d6bad8a44ccf2d48cf328b55
liaowenqiang <wenqiang.liao@liaowenqiangdeMacBook-Pro.local>
2021-10-30 21:40:04 +0800

    [Wenqiang] create reuseable button component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,57 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="content">
+    <h3>Button Components</h3>
+    <div class="button-row">
+      <MyButton /> 
+      <MyButton /> 
+    </div>
+    <div class="button-row">
+      <MyButton variant="outline"/>
+      <MyButton variant="outline"/>
+    </div>
+    <div class="button-row">
+      <MyButton variant="text"/>
+      <MyButton variant="text"/>
+    </div>
+    <div class="button-row">
+      <MyButton disableShadow/>
+    </div>
+    <div class="button-row">
+      <MyButton disabled/>
+      <MyButton variant="text" disabled/>
+    </div>
+    <div class="button-row">
+      <MyButton size="sm"/>
+      <MyButton size="md"/>
+      <MyButton size="lg"/>
+    </div>
+    <div class="button-row">
+      <MyButton color="default"/>
+    </div>
+    <div class="button-row">
+      <MyButton color="primary"/>
+    </div>
+    <div class="button-row">
+      <MyButton color="secondary"/>
+    </div>
+    <div class="button-row">
+      <MyButton color="danger"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyButton from './components/MyButton.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyButton,
   }
 }
 </script>
 
-<style>
+<style lang=less scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +59,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+  .content {
+    text-align: center;
+
+    .button-row {
+      margin-bottom: 50px;
+      display: flex;
+      justify-content: space-evenly;
+    }
+  }
+
+ 
 }
 </style>
